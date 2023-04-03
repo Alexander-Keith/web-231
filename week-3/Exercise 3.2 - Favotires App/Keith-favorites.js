46f6e491c5e2e02d1ee25a243a61389621787c48
Alex <alexanderkeithpro@gmail.com>
2023-04-02 19:28:00 -0500

    Exercise 3.2 - Favorites App

---
+++ week-3/Exercise 3.2 - Favotires App/Keith-favorites.js
@@ -0,0 +1,28 @@
+/*
+=================================================
+;  Title: favorites.js
+;  Author: Alexander Keith
+;  Date: 4/2/2023
+;  Description: favorites.js file for assignment Exercise 3.2 - Favorites App
+;================================================
+*/
+
+// alerts favorite team
+function favoriteTeam() {
+    alert("Alabama Crimson Tide")
+}
+
+// alerts favorite book
+function favoriteBook() {
+    alert("The Hobbit")
+}
+
+// alerts favorite author
+function favoriteAuthor() {
+    alert("JK Rowling")
+}
+
+// alerts favorite programming language
+function favoriteLanguage() {
+    alert("JavaScript")
+}
