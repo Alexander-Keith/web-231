e505243dac3ae1f1eb066e04308882a0aec13753
Alex <alexanderkeithpro@gmail.com>
2023-04-16 20:29:54 -0500

    HTML and JS in work

--- week-5/Keith-sequence-game.js
+++ week-5/Keith-sequence-game.js
@@ -7,46 +7,40 @@
 ;================================================
 */
 
-/*
-//Generates random number between 1 and 10
-const rnd = Math.floor(Math.random() * 10) + 1;
-
-//Declare a function to check guess and variable for submitted guess
-function submit(){
-    const guess = document.getElementById('guessNumber').value;
-
-    //Declare variables  with string values for if the guess was too high, low or correct
-    const msgGreaterThan = "The number is greater than";
-    const msgLessThan= "The number is less than";
-    const msgCongrats = "Congratulations! you picked the correct number!";
-
-    //if, else if, and else decision tree for low, high, and correct guesses
-    if (rnd<guess){
-        document.getElementById("results").innerHTML = msgLessThan + " " + guess;
-    } 
-    
-    else if (rnd>guess){
-        document.getElementById("results").innerHTML = msgGreaterThan + " " + guess;
-    } 
-    
-    else {
-        document.getElementById("results").innerHTML = msgCongrats;
-    }
-}
-*/
-
-//NEW JS
-
 /*one variable, a switch statement, while loop, do...while loop, and a for loop. Used to register the oneclick event for the Display Sequence button in the HTML.
 */
-displaySequence()
+function displaySequence(){
 
-/*variable assigned to the users selection for numbers, even, and odd. "txtNumSequence, txtEvenSequence, txtOddSequence"
+    const choice = getElementById('choice').value;
+
+    switch(choice){
+
+        //Alert message popup when an invalid selection is submitted
+        default: alert("Invalid selection, please try again!")
+        break;
+
+        //Variables and values for index and txtNumSequence
+        case "numbers":
+            let index = 0;
+            //Do we actually need txtNumSequence? couldn't we just add the space to the while loop without this?
+            let txtNumSequence = "";
+
+        //While loop that create 10 iterations to build a comma delimited string that display value 0-10.
+        while (index < 10){
+            txtNumSequence += index = ", ";
+            //This tells the code to increase the value by 1
+            index++;
+        }
+
+    }
+
+  
+
+}
+
+    /*variable assigned to the users selection for numbers, even, and odd. "txtNumSequence, txtEvenSequence, txtOddSequence"
 */
-function submit(){
-    const guess = document.getElementById('guessNumber').value;
+  //  function submit(){
+  //  const guess = document.getElementById('guessNumber').value;
 
-//Switch block with the case statements for numbers, even, and odd
-
-//Alert message popup when an invalid selection is submitted
-alert("Invalid selection, please try again!")
+    //Switch block with the case statements for numbers, even, and odd
