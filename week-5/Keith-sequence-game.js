261f21093733e7bfda047da36a3a0552f84aa000
Alex <alexanderkeithpro@gmail.com>
2023-04-16 22:07:45 -0500

    Completed week 5 work

--- week-5/Keith-sequence-game.js
+++ week-5/Keith-sequence-game.js
@@ -14,6 +14,8 @@ function displaySequence(){
   
     switch(choice){
     
+
+
         //Variables and values for index and txtNumSequence
         case "numbers":
             let index = 0;
@@ -26,19 +28,45 @@ function displaySequence(){
             index++;
         }
         
+        //Puts the JavaScript results to the text element location in the html
         document.getElementById('results').innerHTML = txtNumSequence;
         break;
-  
-      case "even":
-        // code to display even numbers
-        break;
-  
-      case "odd":
-        // code to display odd numbers
+    
+
+
+        //Variables and values for evenNumber and txtEvenSequence
+        case "even":
+            let evenNumber = 0;
+            let txtEvenSequence = "";
+        
+        do {
+            txtEvenSequence += evenNumber + ", "
+            evenNumber += 2;
+        }
+        //While loop that create 10 iterations to build a comma delimited string that display value 0-20.
+        while (evenNumber < 21);
+        
+        //Puts the JavaScript results to the text element location in the html
+        document.getElementById('results').innerHTML = txtEvenSequence;
         break;
 
-    //Alert message popup when an invalid selection is submitted
-    default: alert("Invalid selection, please try again!");
+
+
+        //Variable and value for txtOddSequence
+        case "odd":
+            let txtOddSequence = "";
+
+        for (let i = 1; i <20; i +=2)
+            txtOddSequence += i + ", ";
+
+        document.getElementById('results').innerHTML = txtOddSequence;
+        break;
+    
+
+
+        //Alert message popup when an invalid selection is submitted
+        default: alert("Invalid selection, please try again!");
+
     }
-  }
-  
+
+}
