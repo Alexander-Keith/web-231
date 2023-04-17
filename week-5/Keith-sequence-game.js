c5cbdeb5d00799a696302a4b2c47caa589571eff
Alex <alexanderkeithpro@gmail.com>
2023-04-16 21:16:17 -0500

    fixed earlier issues, only case even and case odd left to do. Internet is spotty right now so I'm pushing just in case

--- week-5/Keith-sequence-game.js
+++ week-5/Keith-sequence-game.js
@@ -1,46 +1,44 @@
 /*
 =================================================
-;  Title: Keith-number-game.js
+;  Title: Keith-sequence-game.js
 ;  Author: Alexander Keith
 ;  Date: 4/15/2023
-;  Description: Keith-number-game.js file for assignment 4.2 - Number Guessing Game
+;  Description: Keith-sequence-game.js file for assignment 5.2 - Sequence Game
 ;================================================
 */
 
 /*one variable, a switch statement, while loop, do...while loop, and a for loop. Used to register the oneclick event for the Display Sequence button in the HTML.
 */
 function displaySequence(){
-
-    const choice = getElementById('choice').value;
-
+    const choice = document.getElementById('choice').value;
+  
     switch(choice){
-
-        //Alert message popup when an invalid selection is submitted
-        default: alert("Invalid selection, please try again!")
-        break;
-
+    
         //Variables and values for index and txtNumSequence
         case "numbers":
             let index = 0;
-            //Do we actually need txtNumSequence? couldn't we just add the space to the while loop without this?
             let txtNumSequence = "";
-
+  
         //While loop that create 10 iterations to build a comma delimited string that display value 0-10.
-        while (index < 10){
-            txtNumSequence += index = ", ";
+        while (index <= 10){
+            txtNumSequence += index + ", ";
             //This tells the code to increase the value by 1
             index++;
         }
-
-    }
-
+        
+        document.getElementById('results').innerHTML = txtNumSequence;
+        break;
   
+      case "even":
+        // code to display even numbers
+        break;
+  
+      case "odd":
+        // code to display odd numbers
+        break;
 
-}
-
-    /*variable assigned to the users selection for numbers, even, and odd. "txtNumSequence, txtEvenSequence, txtOddSequence"
-*/
-  //  function submit(){
-  //  const guess = document.getElementById('guessNumber').value;
-
-    //Switch block with the case statements for numbers, even, and odd
+    //Alert message popup when an invalid selection is submitted
+    default: alert("Invalid selection, please try again!");
+    }
+  }
+  
